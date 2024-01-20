a2d63df0b476135c4cf8187eebef0f3b76c1a717
Stefan Gründel <stefan.gruendel@gmail.com>
2024-01-20 10:45:59 +0100

    fixing Linux tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,7 +4,6 @@ import { suite, test } from 'mocha';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 import * as thExt from '../extension';
 
 suite('Extension Test Suite', () => {
@@ -15,11 +14,6 @@ suite('Extension Test Suite', () => {
     });
 
     test('getThymeleafFragmentsPath()', () => {
-        assert.strictEqual(thExt.getThymeleafFragmentsPath(), 'failing');
-    });
-
-    test('Sample test', () => {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+        assert.strictEqual(thExt.getThymeleafFragmentsPath(), 'src/main/resources/templates');
     });
 });
