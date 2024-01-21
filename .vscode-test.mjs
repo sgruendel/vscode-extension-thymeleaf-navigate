30e5c26aadaacb8477f692811feb8301f4738ce3
Stefan Gründel <stefan.gruendel@gmail.com>
2024-01-21 07:28:49 +0100

    workspace-html

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -5,7 +5,7 @@ export default defineConfig([
         label: 'unitTests',
         files: 'out/test/**/*.test.js',
         version: 'insiders',
-        workspaceFolder: 'src/test/workspace',
+        workspaceFolder: 'src/test/workspace-html',
         mocha: {
             ui: 'tdd',
             timeout: 20000,
