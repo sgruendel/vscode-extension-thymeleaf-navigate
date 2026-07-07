3eadef03a1cf7797b7744a37ceea2316246c910d
Stefan Gründel <stefan.gruendel@gmail.com>
2026-07-07 20:55:12 +0200

    fix: update test configuration so test runs on macOS

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from '@vscode/test-cli';
+import { tmpdir } from 'os';
+import path from 'path';
+
+const workspaceFolder = path.resolve('src/test/workspace-html');
+const userDataDir = path.join(tmpdir(), 'vscode-thymeleaf-navigate-test-user-data');
 
 export default defineConfig([
     {
         label: 'HTML Unit Tests',
         files: 'out/test/**/*.test-html.js',
         version: 'insiders',
-        workspaceFolder: 'src/test/workspace-html',
+        launchArgs: [workspaceFolder, `--user-data-dir=${userDataDir}`],
         mocha: {
             ui: 'tdd',
             timeout: 20000,
